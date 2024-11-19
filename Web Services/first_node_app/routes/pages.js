a0aae7991db225e27756e1d2919e970a5263b177
Mahfuz Ahmed <117375738+mahfuzniloy007@users.noreply.github.com>
2024-11-18 18:11:45 -0700

    1st commit

--- Web Services/first_node_app/routes/pages.js
+++ Web Services/first_node_app/routes/pages.js
@@ -5,12 +5,12 @@ const router = express.Router();
 
 // Define a route for the home page
 router.get('/', (req, res) => {
-    res.send('This is the home page!');
+    res.send(`This is the home page!`);
 });
 
 // Define a route for the about page
 router.get('/about', (req, res) => {
-    res.send('This is the about page!');
+    res.send(`This is the about page!`);
 });
 
 
